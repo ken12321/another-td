4ca3ba40cccbd86fe11f8d8c3751fb2f580010e2
ken12321 <ken.l.h23@gmail.com>
2026-02-27 22:50:37 +1100

    basic tower has a new look and shooting animation

--- scripts/tower.gd
+++ scripts/tower.gd
@@ -2,38 +2,64 @@ extends Node2D
 class_name Tower
 
 @export var data: TowerData
+@onready var animated_sprite = $AnimatedSprite2D
+
+const BULLET_SPAWN_FRAME = 4
 
 var target: Enemy = null
 var enemies_in_range: Array[Enemy] = []
 
-var _timer: Timer
+var _fire_rate_timer: float = 0.0
+#var _timer: Timer
 
 
 # Called when the node enters the scene tree for the first time.
 func setup() -> void:
 	$Range/CollisionShape2D.shape.radius = data.range
 
-	_timer = Timer.new()
-	_timer.wait_time = 1.0 / data.fire_rate
-	_timer.timeout.connect(_shoot)
-	add_child(_timer)
-	_timer.start()
+	animated_sprite.frame_changed.connect(_on_frame_changed)
+	
+	#_timer = Timer.new()
+	#_timer.wait_time = 1.0 / data.fire_rate
+	#_timer.timeout.connect(_shoot)
+	#add_child(_timer)
+	#_timer.start()
 	
 	$Range.body_entered.connect(_on_enemy_entered)
 	$Range.body_exited.connect(_on_enemy_exited)
 
-func _physics_process(_delta: float) -> void:
-	if(target):
-		look_at(target.global_position)
+func _on_frame_changed() -> void:
+	if (animated_sprite.animation == "shooting" && animated_sprite.frame == BULLET_SPAWN_FRAME):
+		_spawn_bullet()
 
-func _shoot() -> void:
+func _process(delta: float) -> void:
 	enemies_in_range = enemies_in_range.filter(func(e): return is_instance_valid(e))
 	target = _get_furthest_enemy()
 	
 	if (!target):
+		animated_sprite.stop()
 		return
-	
-	_spawn_bullet()
+
+	_fire_rate_timer += delta
+	if (_fire_rate_timer >= 1.0 / data.fire_rate):
+		_fire_rate_timer = 0.0
+		_start_shoot_animation()
+
+func _start_shoot_animation() -> void:
+	animated_sprite.speed_scale = data.fire_rate
+	animated_sprite.play("shooting")
+
+#func _shoot() -> void:
+	##animated_sprite.set_animation_speed = 5 # todo fix 5 magic num
+	##animated_sprite.play("shooting")
+	#enemies_in_range = enemies_in_range.filter(func(e): return is_instance_valid(e))
+	#target = _get_furthest_enemy()
+	#
+	#if (!target):
+		##animated_sprite.stop()
+		#return
+	#
+	#_spawn_bullet()
 
 func _on_enemy_entered(body: Node2D) -> void:
 	if (body is Enemy):
@@ -63,7 +89,9 @@ func _spawn_bullet() -> void:
 
 	var bullet := data.bullet_scene.instantiate()
 	bullet.global_position = global_position
-
+	
+	if (!target):
+		return
 	bullet.direction = (target.global_position - global_position).normalized()
 	bullet.speed = 1000
 	bullet.damage = data.damage
